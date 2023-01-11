88c2db18f7d02c527540c89dc9ba47280e1f5ebe
Lucas <lucaslba2105@gmail.com>
2023-01-11 20:59:37 -0300

    Primeiro Commit

--- ControladorAviao/Repository/IVooRepository.cs
+++ ControladorAviao/Repository/IVooRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace ControladorAviao.Repository
+﻿using ControladorAviao.Models;
+
+namespace ControladorAviao.Repository
 {
     public interface IVooRepository
     {
+        IEnumerable<VooModel> Voos { get; }
     }
 }
--- ControladorAviao/Repository/VooRepository.cs
+++ ControladorAviao/Repository/VooRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace ControladorAviao.Repository
+﻿using ControladorAviao.Context;
+using ControladorAviao.Models;
+
+namespace ControladorAviao.Repository
 {
-    public class Interface
+    public class VooRepository : IVooRepository
     {
+        private readonly AppDbContext _context;
+        public VooRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<VooModel> Voos => _context.Voos;
     }
 }
